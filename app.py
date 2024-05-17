6217cf7c2b3e5aec0f082ca20db79367dad85b1d
thomas univ-smb <thomas.durand@etu.univ-smb.fr>
2024-05-17 14:11:37 +0000

    Version 1

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"FMOTDEPASSEz\n\xec]/'
 
 @app.route("/")
 def start():
-    """ Page d'accueil redirigée vers les actualités """
+    """ Page d'acceuil du site web """
+    return redirect(url_for('actualite'))
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """ Permet de se connecter """
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('actualite'))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """ Permet de se déconnecter""" 
+    session.pop('username', None)
     return redirect(url_for('actualite'))
 
 @app.route("/actualites")
@@ -33,6 +48,58 @@ def commentaire():
     """ Valider et écrire un commentaire """
     write_json("commentaire", {'actu': request.form['actu'], 'name': request.form['name'], 'commentaire': request.form['commentaire']})
     return redirect(url_for('actualite'))
+
+@app.route("/ajoutactu", methods=['GET', 'POST'])
+def ajouteractu():
+    """ Permet d'ajouter une actualité """
+    if request.method == 'POST':
+        print(request.form)
+        write_json("actualites", {'id': int(request.form['id']), 'title': request.form['title'], 'dateActu': request.form['dateActu'], 'type' : request.form['type']})
+        return redirect(url_for('actualite'))
+    data=read_json("actualites")
+    max=0
+    index=0
+    for i in range(0,len(data)):
+        if data[i]['id'] > max:
+            max=data[i]['id']
+            index=data[i]['id']+1
+    return '''
+    <form method="post">
+    <input type="hidden" name="id" value='''+str(index)+'''>
+    <p><input disabled value='''+str(index)+''' type=text name=id>
+    <p><input requiered placeholder="Titre de l'actualité" type=text name=title>
+    <p><input requiered placeholder='Type de musique' type=text name=type>
+    <p><label for=dateActu>Date de l'actualité :</label>
+    <input requiered placeholder=dateActu type=date name=dateActu>
+    <p><input type=submit value=Envoyer>
+    </form>
+    '''
+
+@app.route("/ajoutconcert", methods=['GET', 'POST'])
+def ajouterconcert():
+    """ Permet d'ajouter un concert """
+ #{"id": 1, "title": "Musilac 2024", "dateConcert" : "03/05/2024"}
+    if request.method == 'POST':
+        write_json("concert", {'id': int(request.form['id']), 'title': request.form['title'], 'dateConcert': request.form['dateConcert']})
+        return redirect(url_for('concert'))
+    data=read_json("concert")
+    max=0
+    index=0
+    for i in range(0,len(data)):
+        if data[i]['id'] > max:
+            max=data[i]['id']
+            index=data[i]['id']+1
+    return '''
+    <form method="post">
+    <input type="hidden" name="id" value='''+str(index)+'''>
+    <p><input disabled value='''+str(index)+''' type=text name=id>
+    <p><input requiered placeholder="Titre du concert" type=text name=title>
+    <p><label for=dateConcert>Date du concert :</label>
+    <input requiered placeholder=dateConcert type=date name=dateConcert>
+    <p><input type=submit value=Envoyer>
+    </form>
+
+    '''
 
 def read_json(name):
     """ Lire dans un fichier Json """
